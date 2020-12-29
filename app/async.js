84af89186697077b5bede42bae123b8e4b34dd11
Alex Santos <alexsantosquispe@gmail.com>
2020-12-28 23:48:01 -0400

    async-functions and best practices

--- app/async.js
+++ app/async.js
@@ -1,11 +1,22 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(value), 10);
+    });
   },
 
-  manipulateRemoteData: function(url) {
+  manipulateRemoteData: function (url) {
+    var dfd = $.Deferred();
 
-  }
+    $.ajax(url).then(function (resp) {
+      var people = $.map(resp.people, function (person) {
+        return person.name;
+      });
+      dfd.resolve(people.sort());
+    });
+
+    return dfd.promise();
+  },
 };
